c566343951a79953fb59dd523bfaeb5798eac32f
Jesus M. Castagnetto <jesus@castagnetto.com>
2022-08-23 10:06:05 -0500

    updated with data downloaded on 2022-08-23

--- genfigstables.sh
+++ genfigstables.sh
@@ -15,7 +15,7 @@ Rscript fig04-monkeypox-plot-incidence-by-subregion.R
 echo ">>> LATAM & Caribbean incidence table"
 Rscript tab01-monkeypox-tables-incidence.R
 echo ">>> Duplication time and Rt plot"
-Rscript fig05ab-monkeypox-duplication-time-plot.R
+Rscript fig05ab-monkeypox-td-rt-plot.R
 echo ">>> Supplementary materials PDF"
 quarto render paper-supplementary-materials.qmd --quiet --to pdf
 ls -lh  paper-supplementary-materials.pdf
